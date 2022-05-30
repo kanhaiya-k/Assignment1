543841a578aaa1d7c4b8f840b6d7999bffcdacd9
kanhaiya kumar <kanhaiya.iksula@gmail.com>
2022-05-30 08:50:43 +0530

    first commit

---
+++ React-sy/README.md
@@ -0,0 +1,45 @@
+# Syngenta-ReactJS-Balloon-Assignment
+
+### To Checkout the live demo of this Project <a href="https://syngenta-reactjs-balloon-assignment.netlify.app/">Click Here</a>
+
+### Problem Statement:~ [ReactJS-Balloon-Assignment.pdf](https://github.com/Satyamall/Syngenta-ReactJS-Balloon-Assignment/files/8792893/ReactJS-Balloon-Assignment.pdf)
+Create an empty box, 5 circles each of unique colours. The colour generation method should randomly pick and assign one of the five colours to the circles. A text box that will take numeric input and a button named “shoot”.
+ 
+  - A user can enter a number in the text box and click shoot button. This would move the corresponding circle into the empty div.
+  - The user can get back the circles that were shot by clicking the corresponding circles residing in the empty div.
+  
+
+### Steps to run this project:~
+#### Open your terminal and navigate to a folder where you want to run this project:
+ - Step.1 clone this repository in your system using the following command
+ ```js
+ git clone https://github.com/Satyamall/Syngenta-ReactJS-Balloon-Assignment.git
+ ```
+- Step.2 Now navigate inside the project directory using the following command 
+```js
+ cd Syngenta-ReactJS-Balloon-Assignment
+```
+- Step.3 Install the required packages to run this project using the following command 
+```js
+ npm install
+```
+- Step.4 To run this project run the following command into your terminal 
+```js
+ npm run start
+```
+
+ # Instruction for use: 
+  - Type the number between 1 to 5 in Text box and click on Shoot button. You able to see that circle show inside the empty box.
+  - You want to remove circle from empty box and send it to original position then **you click on that circle inside empty box**. You will be able to see it move back to its original position.
+
+<br/>
+
+### Images of Assignment how it looks like and how it works.
+
+![Screenshot (443)](https://user-images.githubusercontent.com/80479635/170857445-f6cf3277-e020-410d-996c-4dc65ca3ac32.png)
+![Screenshot (444)](https://user-images.githubusercontent.com/80479635/170857449-03491de2-c706-46ea-a00a-4e699b00e331.png)
+![Screenshot (445)](https://user-images.githubusercontent.com/80479635/170857453-c145f3fe-3f35-4f94-a3b7-b544c451d653.png)
+![Screenshot (446)](https://user-images.githubusercontent.com/80479635/170857450-863afd9b-068e-4ef1-8894-2123945dbec3.png)
+![Screenshot (447)](https://user-images.githubusercontent.com/80479635/170857454-12052f42-f6db-4d1e-9329-0622f822797d.png)
+
+
---
+++ React-sy/src/App.js
@@ -0,0 +1,101 @@
+
+import './App.css';
+import { useState } from "react";
+import randomcolor from 'randomcolor';
+
+// Total number circles inside an array with id, randomcolor(),and show options.
+const totalCircles = [
+  {
+    id: 1,
+    color: randomcolor(),
+    show: true
+  },
+  {
+    id: 2,
+    color: randomcolor(),
+    show: true
+  },
+  {
+    id: 3,
+    color: randomcolor(),
+    show: true
+  },
+  {
+    id: 4,
+    color: randomcolor(),
+    show: true
+  },
+  {
+    id: 5,
+    color: randomcolor(),
+    show: true
+  }
+]
+
+function App() {
+
+  // it contains the total circles
+  const [circles, setCircles] = useState(totalCircles)
+
+  // it contain the input text in numeric value
+  const [text, setText] = useState(null);
+
+  // This function show circles inside the empty box
+  const handleShowInEmptyBox = () => {
+    const showCircleToEmptyBox = circles.map((circle) => circle.id === Number(text) ? { ...circle, show: false } : circle)
+    setCircles(showCircleToEmptyBox);
+  }
+
+  // This function hide the circle from empty box and show the circle on its original position.
+  const handleRemoveCricle = (id) => {
+    const removeCircleFromEmptyBox = circles.map((circle) => circle.id === Number(id) ? { ...circle, show: true } : circle)
+    setCircles(removeCircleFromEmptyBox);
+  }
+
+  return (
+    <div className="App">
+      <h1>Syngenta +Assignment</h1>
+
+      {/* This is Main Box which contains hole code for all operations of circles */}
+      <div className='main-box'>
+
+        {/* This is Empty box code to show and remove circle inside it  */}
+        <div className='box'>
+          <h3>Empty Box</h3>
+          <div className='empty-box'>
+            {circles?.filter((v) => v.show === false).map((circle) => {
+              return <button key={circle.id}
+                className='circle'
+                style={{ background: `${circle.color}` }}
+                onClick={() => handleRemoveCricle(circle.id)}
+              ></button>
+            })
+            }
+          </div>
+        </div>
+
+        {/* This is 5 circles box which contains the circles */}
+        <div className='box'>
+          <h3>{circles.length} Circles</h3>
+          {circles?.filter((v) => v.show === true).map((circle) => {
+            return <div key={circle.id} className='circle' style={{ background: `${circle.color}` }}></div>
+          })
+          }
+        </div>
+
+        {/* This Box contains the input text which take number as input and shoot button */}
+        <div className='box'>
+          <h3>A Text Box</h3>
+          <input type="number" placeholder="Enter number between 1 to 5" min="1" max={circles.length} className='input-box'
+            onChange={(e) => setText(e.target.value)}
+          />
+          <div>
+            <button className='btn' onClick={handleShowInEmptyBox}>Shoot</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
